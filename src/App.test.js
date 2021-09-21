7f6faec7c51de3d81f5e0e09881f88c577d55022
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-09-21 22:40:17 +0300

    fixed some things beore submissions

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
-import Login from "./components/login/login";
+
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Supermetrics Assignment/);
