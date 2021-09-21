51631df3df56520cfc54823429842ef3e32b053f
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-09-21 22:42:56 +0300

    ready to deploy

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders elements in App component", () => {
   render(<App />);
   const linkElement = screen.getByText(/Supermetrics Assignment/);
   expect(linkElement).toBeInTheDocument();
